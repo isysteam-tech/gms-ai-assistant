d6c4864d75d531a0756fa6399bf03fd20c7c8d04
satz15 <sathishsekar154@gmail.com>
2025-10-15 19:07:46 +0530

    tailwind work fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center min-h-screen bg-black">
       <h1 className="text-4xl font-bold text-blue-300">Hello GMS AI-Assistant</h1>
     </div>
     </>
